8a312facbcdef7783b9e8445ebe4dfd127ef8c68
Tyom00 <khachatryantyom02@gmail.com>
2024-02-17 18:31:16 +0400

    Initial Commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,43 @@
 const wrapper = document.createElement("div");
 wrapper.classList.add("wrapper");
+document.body.append(wrapper);
+
+const board = document.createElement("div");
+board.classList.add("board");
+wrapper.append(board);
+
+for (let y = 0; y < 3; y++) {
+    
+    const rowElement = document.createElement("div");
+    rowElement.classList.add("row");
+
+    for (let x = 0; x < 3; x++) {
+
+        const box = document.createElement("div");
+        box.classList.add("box");
+
+        const button = document.createElement("button");
+        button.classList.add("button");
+        box.append(button);
+
+        button.addEventListener("click", (e) => {
+            
+            if (window.character === undefined) {
+                button.innerText = "X"
+            }
+            else if (window.character === "X") {
+                button.innerText = "O"
+            } else {
+                button.innerText = "X"
+            }
+
+            window.character = e.target.innerText
+        })
+
+        rowElement.append(box);
+
+    }
+
+    board.append(rowElement);
+
+}
